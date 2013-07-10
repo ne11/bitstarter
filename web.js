8a4681a95d9e3871ca5007e9c1eef9abb86b8d30
Ubuntu <flowing4@gmail.com>
2013-07-10 15:36:55 +0000

    Not working, but index.html added

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var buff = new Buffer (fs.readFileSync("index.html", "utf-8"));
+  fs.writeFile(buff.toString("utf-8"), function (err) {
+    if (err) throw err;
+  });    
 });
 
 var port = process.env.PORT || 5000;
