c64775a441f5c8b478003df04d08ef425a22fa2b
Ubuntu <flowing4@gmail.com>
2013-07-12 07:53:14 +0000

    test part 1

--- web.js
+++ web.js
@@ -1,16 +1,17 @@
+#!/usr/bin/env node
+
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express.createserver(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buff = new Buffer (fs.readFileSync("index.html", "utf-8"));
-  fs.writeFile(buff.toString("utf-8"), function (err) {
-    if (err) throw err;
-  });    
+  var bufindex = fs.readfilesync("index.html", "utf-8");
+  fs.writefile(bufindex.tostring());
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
