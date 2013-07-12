cafdfee06cbe8f4ef55ff05eb2f70e30c691f341
Ubuntu <flowing4@gmail.com>
2013-07-12 08:30:27 +0000

    Part 1 v3

--- web.js
+++ web.js
@@ -5,10 +5,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  console.log("in get loop");  
+  console.log("in get loop");
   var fs = require('fs');
-  var bufindex = fs.readfilesync("index.html", "utf-8");
-  response.send(fs.writefile(bufindex.tostring()));
+  var bufindex = fs.readFileSync("index.html", "utf-8");
+  response.send(fs.writeFile(bufindex.tostring()));
 });
 
 var port = process.env.PORT || 5000;
